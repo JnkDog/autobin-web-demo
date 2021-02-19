e19697ba4f519f1563846c7f68cf6552d54f305f
fzzfs <530661401@qq.com>
2021-02-19 18:51:38 +0800

    make Chart component connecting to db

--- src/components/PieChart/PieChart.js
+++ src/components/PieChart/PieChart.js
@@ -1,52 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import { Pie } from '@ant-design/charts';
 
-// to do
-// fetchDAta from database
-const data = [
-    {
-        type: 'Recyclable Waste',
-        value: 27,
-    },
-    {
-        type: 'Residual Waste',
-        value: 25,
-    },
-    {
-        type: 'Food Waste',
-        value: 18,
-    },
-    {
-        type: 'Hazardous Waste',
-        value: 15,
-    }
-];
-
-// Pie Chart's config
-const config = {
-    appendPadding: 10, 
-    autoFit: true,
-    data: data,
-    angleField: 'value',
-    colorField: 'type',
-    color: ['#002EB8', '#1F1F1F', '#663300', '#B8002E'],
-    radius: 0.9,
-    label: {
-      type: 'inner',
-      offset: '-30%',
-      content: function content(_ref) {
-        var percent = _ref.percent;
-        return ''.concat(Math.floor(percent * 100), '%');
-      },
-      style: {
-        fontSize: 14,
-        textAlign: 'center',
-      },
-    },
-    interactions: [{ type: 'element-active' }],
-};
+URL = 'http://localhost:5000/records/piechart';
 
 const PieChart = () => {
+    const[data, setData] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+          const { data } = await axios.get(URL);
+          setData(data);
+        }
+  
+        fetchData();
+    }, []);
+
+    // Pie Chart's config
+    const config = {
+        appendPadding: 10, 
+        autoFit: true,
+        data: data,
+        angleField: 'value',
+        colorField: 'type',
+        color: ['#002EB8', '#1F1F1F', '#663300', '#B8002E'],
+        radius: 0.9,
+        label: {
+        type: 'inner',
+        offset: '-30%',
+        content: function content(_ref) {
+            var percent = _ref.percent;
+            return ''.concat(Math.floor(percent * 100), '%');
+        },
+        style: {
+            fontSize: 14,
+            textAlign: 'center',
+        },
+        },
+        interactions: [{ type: 'element-active' }],
+    };
+
     return <Pie {...config} />;
 };
   
